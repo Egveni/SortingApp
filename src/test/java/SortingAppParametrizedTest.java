4a155f37e094394098a2d8f990bb054297c0b3d0
Evgenii Goncharov <Eugeni.goncharov@gmail.com>
2022-09-29 15:17:54 +0400

    .

--- src/test/java/SortingAppParametrizedTest.java
+++ src/test/java/SortingAppParametrizedTest.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class SortingAppParametrizedTest {
+import com.epam.SortingApp;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class SortingAppParametrizedTest {
+    private final int[] array;
+    private final int[] expectedArray;
+
+    public SortingAppParametrizedTest(int[] array, int[] expectedArray) {
+        this.array = array;
+        this.expectedArray = expectedArray;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{0, -1, -1, 5, 8, 9}, new int[]{-1, -1, 0, 5, 8, 9}},
+                {new int[]{3, -5, -7, 44, 2, 5}, new int[]{-7, -5, 2, 3, 5, 44}},
+                {new int[]{0, -2, 3, 69, 19, 1}, new int[]{-2, 0, 1, 3, 19, 69}},
+                {new int[]{1, 3, 2}, new int[]{1, 2, 3}},
+                {new int[]{3, 2, 1}, new int[]{1, 2, 3}},
+                {new int[]{1, 2, 3}, new int[]{1, 2, 3}},
+                {new int[]{1, 1, 1}, new int[]{1, 1, 1}},
+                { new int[] { 1 }, new int[] { 1 } },
+        });
+    }
+
+    @Test
+    public void testDifferentCases() {
+        SortingApp sortingApp = new SortingApp();
+        sortingApp.mergeSort(array);
+        Assert.assertArrayEquals(expectedArray, array);
+    }
+
 }
--- src/test/java/SortingAppTest.java
+++ src/test/java/SortingAppTest.java
@@ -1,3 +1,47 @@
-public class TestSortingApp {
+import com.epam.SortingApp;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+public class SortingAppTest {
+    private static SortingApp sortingApp;
+
+    @BeforeClass
+    public static void createNewSortingApp() {
+
+        sortingApp = new SortingApp();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullCaseShouldThrowException() {
+
+        sortingApp.mergeSort(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void emptyArrayShouldThrowAnException() {
+        int[] arr = {};
+        sortingApp.mergeSort(arr);
+    }
+
+    @Test
+    public void arrayAlreadySortedShouldBeTheSame() {
+        int[] arr = new int[]{-1, -1, 0, 5, 8, 9};
+        int[] expectedArr = new int[]{-1, -1, 0, 5, 8, 9};
+        Assert.assertArrayEquals(expectedArr, sortingApp.mergeSort(arr));
+    }
+
+    @Test
+    public void arrayWithOneElementShouldBeSorted() {
+        int[] arr = new int[]{9};
+        int[] expectedArr = new int[]{9};
+        Assert.assertArrayEquals(expectedArr, sortingApp.mergeSort(arr));
+    }
+
+    @Test
+    public void arrayShouldBeSorted() {
+        int[] arr = {0, -2, 3, 69, 19, 1};
+        int[] expectedArr = {-2, 0, 1, 3, 19, 69};
+        Assert.assertArrayEquals(expectedArr, sortingApp.mergeSort(arr));
+    }
 }
